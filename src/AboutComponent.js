eb8989885b59c5bbb510e759d924322c50691941
asearer <amsearer84@gmail.com>
2024-03-13 14:55:56 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/AboutComponent.js

--- src/AboutComponent.js
+++ src/AboutComponent.js
@@ -5,9 +5,9 @@ const AboutComponent = () => {
   return (
     <div className="page-container">
       <div className="content">
-        <h1>About Page</h1>
-        <p>Welcome to our app's About page!</p>
-        <p>The Ethiria Card Explorer is designed to be a companion app that inspires interest and feedback regarding the upcoming game Chronicles of Ethiria.</p>
+        <h1>About </h1>
+        <p>Welcome to the app's About page!</p>
+        <p>The Ethiria Card Explorer is designed to be a promotional companion app that inspires interest and feedback regarding the upcoming game Chronicles of Ethiria.</p>
         <p>Feel free to browse through the demo cards provided in the app, and please don't hesitate to provide any suggestions or feedback that you might have using the subject 'Ethiria'!</p>
       </div>
     </div>
